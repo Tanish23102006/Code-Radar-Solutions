ff0e4eda445a59883e28fddbef10c32c964a4f53
Tanish23102006 <tanish0234.becse24@chitkara.edu.in>
2025-01-29 10:03:55 +0530

    Solution to Grade Based on Marks

--- Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -7,5 +7,17 @@ int main() {
     if(x>=90){
         printf("A");
     }
+    else if(x>=80){
+        printf("B");
+    }
+    else if(x>=70){
+        printf("C");
+    }
+    else if(x>=60){
+        printf("D");
+    }
+    else{
+        printf("F");
+    }
     return 0;
 }
